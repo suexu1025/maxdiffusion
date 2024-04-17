da5ca444a8efbfa5a6d05ef86fd645e723a02cd1
Qinwen Xu <qinwen@google.com>
2024-04-17 19:52:04 +0000

    update eval

--- xpk/v5p-32_eval.sh
+++ xpk/v5p-32_eval.sh
@@ -62,13 +62,14 @@ export LIBTPU_INIT_ARGS='--xla_tpu_enable_async_collective_fusion_fuse_all_gathe
 
 
 #cd /maxdiffusion
-git clone -b  mlperf_4  https://github.com/suexu1025/maxdiffusion.git maxdiffusion
+git clone -b  mlperf_4_fix_pad  https://github.com/suexu1025/maxdiffusion.git maxdiffusion 
 cd maxdiffusion
+#gsutil cp gs://test-example-123/sd/src/generate-shard.py src/maxdiffusion/generate.py
 
 pip install .
 mkdir generated_images
 mkdir output
 
-TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TF_CPP_MIN_LOG_LEVEL=0 python -m src.maxdiffusion.eval src/maxdiffusion/configs/base_2_base.yml run_name=v5p-128-eval per_device_batch_size=16 base_output_directory=${OUT_DIR}
+TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TF_CPP_MIN_LOG_LEVEL=0 python -m src.maxdiffusion.eval src/maxdiffusion/configs/base_2_base.yml run_name=v5p-128-eval per_device_batch_size=16 caption_coco_file=/app/datasets/coco2014/val2014_30k_padded.tsv images_directory=/app/maxdiffusion/generated_images/ base_output_directory=${OUT_DIR}
 
 #gsutil cp -r generated_images ${OUT_DIR}/output/
